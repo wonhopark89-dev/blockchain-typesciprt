c72962189a9a982370b8b1c9607d356b3d58e87c
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-08-05 00:07:28 +0900

    how to use class

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,24 @@ const sayHi = (tomato:Human):string => {
 
 console.log(sayHi(person));
 
+// class
+class Human2 {
+   public name:string;
+   public age:number;
+   public gender:string
+   constructor(name:string, age:number, gender:string) {
+      this.name = name;
+      this.age = age;
+      this.gender = gender ;
+   }
+}
+
+const person2 = new Human2("soooo", 24, "female");
+
+const sayHi2 = (potato:Human2):string => {
+   return `${potato.name} ${potato.age} ${potato.gender}`;
+}
+
+console.log(sayHi2(person2));
+
 export {}
