fb36dd664819d3b24767298b9aa71c6552f2e224
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-08-04 23:56:10 +0900

    how to use interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-   return `${name} ${age} ${gender}`;
+interface Human {
+   name:string,
+   age:number,
+   gender:string
 }
 
-console.log(sayHi("wonho",32, "male"));
+const person = {
+   name:"wonho",
+   age:22,
+   gender:"male"
+}
+const sayHi = (tomato:Human):string => {
+   return `${tomato.name} ${tomato.age} ${tomato.gender}`;
+}
+
+console.log(sayHi(person));
 
 export {}
